2a0929fec755b0f468acf26842e8027775de0dc0
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:27:02 +0530

    initial commit

---
+++ src/main/java/com/crowdseats/analytics_service/CsAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crowdseats.analytics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsAnalyticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsAnalyticsServiceApplication.class, args);
+	}
+
+}
