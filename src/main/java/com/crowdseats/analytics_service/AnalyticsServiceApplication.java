a57c46bba5a240e92d8ad83bbc2bbfbbeb30af56
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:48:02 +0530

    init commit 2

--- src/main/java/com/crowdseats/analytics_service/AnalyticsServiceApplication.java
+++ src/main/java/com/crowdseats/analytics_service/AnalyticsServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsAnalyticsServiceApplication {
+public class AnalyticsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CsAnalyticsServiceApplication.class, args);
+		SpringApplication.run(AnalyticsServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/crowdseats/analytics_service/AnalyticsServiceApplicationTests.java
+++ src/test/java/com/crowdseats/analytics_service/AnalyticsServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CsAnalyticsServiceApplicationTests {
+class AnalyticsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
